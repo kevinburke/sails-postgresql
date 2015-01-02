6904673dde8e41ed9b5600e5865715ed72a63833
Kevin Burke <kev@inburke.com>
2015-01-02 11:04:43 -0800

    specify user/database, run on circle

--- test/integration/runner.js
+++ test/integration/runner.js
@@ -70,7 +70,7 @@ new TestRunner({
     host: 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
-    database: 'sailspg',
+    database: process.env.DB_DATABASE || 'sailspg',
     port: 5432,
     schema: true,
     ssl: false
